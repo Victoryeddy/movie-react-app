d99061508956eceddd1fe86f30a8d431ba9a52e8
Victory Ndu <victor5ndu@gmail.com>
2024-12-08 23:55:24 +0100

    Add side menus

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 
-import Love from "../../assets/love.svg"
-import Home from "../../assets/home.svg"
+import Love from "../../assets/svg/love.svg";
+import Home from "../../assets/svg/home.svg";
 
-export const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export const Navbar = ({ onEmit }) => {
+
+  const emitSidebar = () => {
+    onEmit();
+  };
 
   return (
     <nav className="bg-gray-800 text-white sticky top-0 lg:z-50">
@@ -39,11 +42,8 @@ export const Navbar = () => {
           </div>
 
           {/* Mobile Menu Button */}
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            >
+          <div className="lg:hidden">
+            <button onClick={() => emitSidebar()}>
               <svg
                 className="h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -52,47 +52,19 @@ export const Navbar = () => {
                 stroke="currentColor"
                 aria-hidden="true"
               >
-                {isOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16m-7 6h7"
-                  />
-                )}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
               </svg>
             </button>
           </div>
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      {isOpen && (
-        <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link
-              to="/"
-              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
-            >
-              Home
-            </Link>
-            <Link
-              to="/favorites"
-              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
-            >
-              Favorite Movies
-            </Link>
-          </div>
-        </div>
-      )}
+    
     </nav>
   );
 };
-
--- src/Layouts/MovieLayout/MovieLayout.jsx
+++ src/Layouts/MovieLayout/MovieLayout.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { Navbar } from "../../Components/Navbar/Navbar";
 
+import Popular from "../../assets/svg/popular.svg";
+import TopRated from "../../assets/svg/star.svg";
+import Upcoming from "../../assets/svg/computer.svg";
+
+
+import Action from "../../assets/svg/action.svg";
+import Adventure from "../../assets/svg/adventure.svg";
+import Animation from "../../assets/svg/animation.svg";
+import Comedy from "../../assets/svg/comedy.svg";
+import Crime from "../../assets/svg/crime.svg";
+import Documentary from "../../assets/svg/documentary.svg";
+
 const MovieLayout = ({ children }) => {
   const [isAsideOpen, setIsAsideOpen] = useState(false);
 
@@ -8,31 +20,66 @@ const MovieLayout = ({ children }) => {
     setIsAsideOpen(!isAsideOpen);
   };
 
+  const [menuCategories, setMenuCategories] = useState([
+    {id: 1, icon: `${Popular}`, menuName: 'Popular'},
+    {id: 2, icon: `${TopRated}`, menuName: 'Top Rated'},
+    {id: 3, icon: `${Upcoming}`, menuName: 'Upcoming'},
+  ])
+
+  const [genres, setGenres] = useState([
+    {id: 1, icon: `${Action}`, menuName: 'Action'},
+    {id: 2, icon: `${Adventure}`, menuName: 'Adventure'},
+    {id: 3, icon: `${Animation}`, menuName: 'Animation'},
+    {id: 4, icon: `${Comedy}`, menuName: 'Comedy'},
+    {id: 5, icon: `${Crime}`, menuName: 'Crime'},
+    {id: 6, icon: `${Documentary}`, menuName: 'Documentary'},
+  ])
+
   return (
     <>
       {/* Navbar */}
-      <Navbar />
+      <Navbar onEmit={toggleAside} />
 
       {/* Layout Wrapper */}
       <div className="flex flex-col lg:flex-row relative">
-        {/* Aside - Hidden on small screens, toggled visibility */}
-        <button
-          className="block text-white mb-4 bg-blue-900 lg:hidden"
-          onClick={toggleAside}
-        >
-          Close Menu
-        </button>
         <aside
           className={`fixed lg:sticky top-0 bg-[#EAEFBD] p-4 lg:z-90 z-20 
           ${isAsideOpen ? "left-0" : "-left-full"} 
           lg:left-0 transition-all duration-300 lg:w-[20%] 
           w-[80%] h-full lg:h-auto`}
         >
-          <ul className="text-white">
-            <li className="text-black">Home</li>
-            <li className="text-black">Favorites</li>
-            <li className="text-black">Categories</li>
-          </ul>
+          <div>
+            <h3>Categories</h3>
+            <ul className="text-white">
+              {menuCategories.map((item, index) => (
+                <li
+                  className={`text-black p-2 font-light flex items-center hover:bg-gray-300 rounded-lg hover:ms-3  ${
+                    item.id == 1 ? "mt-3" : "mt-0"
+                  } `}
+                  key={item.id}
+                >
+                  <img src={item.icon} alt="" className="me-1" />
+                  {item.menuName}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h3 className="mt-4">Genres</h3>
+            <ul className="text-white">
+              {genres.map((item, index) => (
+                <li
+                  className={`text-black p-2 font-light flex items-center hover:bg-gray-300 rounded-lg hover:ms-3  ${
+                    item.id == 1 ? "mt-2" : "mt-0"
+                  } `}
+                  key={item.id}
+                >
+                  <img src={item.icon} alt="" className="me-1" />
+                  {item.menuName}
+                </li>
+              ))}
+            </ul>
+          </div>
         </aside>
 
         {/* Main Content */}
